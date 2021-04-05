fefed58c68310907086c2c33fd675422270758ba
Nathaniel Potrepka <nspotrepka@gmail.com>
2021-04-05 09:51:16 -0400

    Refactor server start code

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ import app from './app';
 /**
  * Start Express server.
  */
-const server = app.listen(app.get('port'), () => {
-  console.log('  App is running at http://localhost:%d in %s mode',
-              app.get('port'),
-              app.get('env'));
+const port = app.get('port');
+const env = app.get('env');
+const server = app.listen(port, () => {
+  console.log('  App is running at http://localhost:%d in %s mode', port, env);
   console.log('  Press CTRL-C to stop\n');
 });
 
