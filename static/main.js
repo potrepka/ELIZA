ba744fc0597733c798349d34cdb11324d1c976a3
Nathaniel Potrepka <nspotrepka@gmail.com>
2021-04-05 07:43:24 -0400

    Add chatbot image

--- static/main.js
+++ static/main.js
@@ -20,9 +20,7 @@ const formatDate = (date) => {
 const addSenderMessage = (message, time = '12:00 PM | April 1') => {
     $chat.prepend(`
         <div class="media w-33 mb-2">
-            <img
-                src="https://s3.amazonaws.com/com.getstoryflow.api.images/1613581528795-app-icons-big-sur-type-2-v3.png" alt="user" width="50"
-                class="rounded-circle" />
+            <img src="./chatbot.png" width="50" class="rounded-circle" />
             <div class="media-body ml-3">
                 <div class="bg-light rounded py-2 px-3 mb-2">
                     <p class="text-small mb-0 text-muted">${message}</p>
